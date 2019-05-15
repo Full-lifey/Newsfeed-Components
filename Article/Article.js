afa45d63b8fdb4bff80950554934de7eb8297c5c
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-05-15 16:37:41 -0600

    STRETCH: Mark as Read button completed

--- Article/Article.js
+++ Article/Article.js
@@ -8,16 +8,26 @@ class Article {
     this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'Click to Expand';
+    // Set reference to read button
+    this.readButton = domElement.querySelector('.readButton')
+    // Set the text on the read button to say 'Mark as Read'
+    this.readButton.textContent = 'Mark as Read'
+    // Set a click handler on readButton, hiding the article.
+    this.readButton.addEventListener('click', event => {
+      // this.domElement.style.display = 'none'
+      TweenMax.to(this.domElement, 1, {opacity: '0'})
+      TweenMax.to(this.domElement, 0, {display:'none', delay:1})
+    })
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle())
-    console.log(this.domElement, this.expandButton)
+    // console.log(this.domElement, this.expandButton)
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     // this.domElement.classList.toggle('article-open')
     if (this.expandButton.textContent === 'Click to Expand'){
-      TweenMax.to(this.domElement, 1, {maxHeight:500})
+      TweenMax.to(this.domElement, 1, {maxHeight:600})
       this.expandButton.textContent = 'Click to Close'
     } else if (this.expandButton.textContent === 'Click to Close'){
       TweenMax.to(this.domElement, 1, {maxHeight:50})
